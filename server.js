cdc9ff8e2096857daad98c1a3fc466a914137de8
KaoriYes <Quinten.kok@hva.nl>
2023-02-23 15:03:17 +0100

    form added be

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const chalk = require("chalk");
 
+const bodyParser = require('body-parser');
+
 const host = "localhost";
 const port = 420;
 
@@ -10,20 +12,43 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send("Hello World!");
-})
+});
 
 app.get('/about', (req, res) => {
   res.send("About Page");
-})
+});
 app.get('/contact', (req, res) => {
   res.send("Contact Page");
-})
+});
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.post('/submit', (req, res) =>{
+//   const name = req.body.name;
+//   const email = req.body.email;
+//   res.send('Name: ${name}, Email: ${email}');
+// }); 
+
+// app.get('/form', (req, res) => {
+//   res.render('form.ejs');
+// });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/form', (req, res) => {
+  res.render('form.ejs');
+});
+
+app.post('/submit', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  res.send(`Name: ${name}, Email: ${email}`);
+});
 
 app.use(function(req, res){
   res.status(404).render('404', { path: 'Error'});
-})
+});
 
 app.listen(port, () => {
   console.log(chalk.green(`Server is running on http://${host}:${port}`));
-})
+});
 
